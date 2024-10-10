4ac903ae821d6f8f8fddf4c4d2502dde01c485e0
kirillhorkov <kirillnb72@icloud.com>
2024-10-10 10:24:13 +0300

    Added search max.

--- сапр/2.py
+++ сапр/2.py
@@ -2,12 +2,19 @@ import json
 from tools import Matrix
 
 
-def gauss_method(matrix: Matrix) -> Matrix:
-    n = matrix.shape[0]
+def gauss_method(matrix: list[list[float]]) -> list[list[float]]:
+    n = len(matrix)
+
     for i in range(n):
+        max_row = max(range(i, n), key=lambda r: abs(matrix[r][i]))
+
+        if i != max_row:
+            matrix[i], matrix[max_row] = matrix[max_row], matrix[i]
+
         pivot = matrix[i][i]
         if pivot == 0:
             raise ValueError("Матрица вырожденная, нет единственного решения")
+
         matrix[i] = [m / pivot for m in matrix[i]]
 
         for j in range(i + 1, n):
@@ -34,25 +41,21 @@ def LU_method(matrix: Matrix, b: list) -> list:
     U = Matrix([[0.0] * n for _ in range(n)])
 
     for i in range(n):
-        # Формируем верхнетреугольную матрицу U
         for k in range(i, n):
             sum_upper = sum(L[i][j] * U[j][k] for j in range(i))
             U[i][k] = matrix[i][k] - sum_upper
 
-        # Формируем нижнетреугольную матрицу L
         for k in range(i, n):
             if i == k:
-                L[i][i] = 1  # Диагональ как 1
+                L[i][i] = 1
             else:
                 sum_lower = sum(L[k][j] * U[j][i] for j in range(i))
                 L[k][i] = (matrix[k][i] - sum_lower) / U[i][i]
 
-    # Решаем Ly = b
     y = [0.0] * n
     for i in range(n):
         y[i] = b[i] - sum(L[i][j] * y[j] for j in range(i))
 
-    # Решаем Ux = y
     x = [0.0] * n
     for i in range(n - 1, -1, -1):
         x[i] = (y[i] - sum(U[i][j] * x[j] for j in range(i + 1, n))) / U[i][i]
@@ -60,7 +63,6 @@ def LU_method(matrix: Matrix, b: list) -> list:
     return x
 
 
-# Пример использования
 tasks = json.load(open('answers/2.json', 'r', encoding='utf-8'))
 
 number = input('Введите вариант задания: ')
